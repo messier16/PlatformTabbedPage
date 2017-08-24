b5d47b5841b14da57c5657ac289dc817c7d121c9
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-08-24 13:48:57 -0500

    Android renderer & support extensions class

---
+++ PlatformTabbedPage.Droid/PlatformTabbedPageRenderer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Graphics;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+using Messier16.Forms.Controls.Droid;
+using Messier16.Forms.Controls;
+using Android.Graphics.Drawables;
+
+[assembly: ExportRenderer(typeof(PlatformTabbedPage), typeof(PlatformTabbedPageRenderer))]
+namespace Messier16.Forms.Controls.Droid
+{
+    public class PlatformTabbedPageRenderer : TabbedPageRenderer
+    {
+        public static void Init()
+        {
+            var unused = DateTime.UtcNow;
+        }
+
+        private PlatformTabbedPage FormsTabbedPage => Element as PlatformTabbedPage;
+        private Android.Graphics.Color _selectedColor = Android.Graphics.Color.Black;
+        private static readonly Android.Graphics.Color DefaultUnselectedColor = Xamarin.Forms.Color.Gray.Darken().ToAndroid();
+        private static Android.Graphics.Color _barBackgroundDefault;
+        private Android.Graphics.Color _unselectedColor = DefaultUnselectedColor;
+
+        ViewPager _viewPager;
+        TabLayout _tabLayout;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+
+            base.OnElementChanged(e);
+
+            // Get tabs
+            for (var i = 0; i < ChildCount; i++)
+            {
+                var v = GetChildAt(i);
+                var pager = v as ViewPager;
+                if (pager != null)
+                    _viewPager = pager;
+                else if (v is TabLayout)
+                    _tabLayout = (TabLayout)v;
+            }
+
+
+            if (e.OldElement != null)
+            {
+                _tabLayout.TabSelected -= TabLayout_TabSelected;
+                _tabLayout.TabUnselected -= TabLayout_TabUnselected;
+            }
+
+            if (e.NewElement != null)
+            {
+                _barBackgroundDefault = (_tabLayout.Background as ColorDrawable)?.Color ??
+                    Android.Graphics.Color.Blue;
+                SetSelectedColor();
+                SetBarBackgroundColor();
+                _tabLayout.TabSelected += TabLayout_TabSelected;
+                _tabLayout.TabUnselected += TabLayout_TabUnselected;
+
+                SetupTabColors();
+                SelectTab(0);
+            }
+
+        }
+
+        void SelectTab(int position)
+        {
+            if (_tabLayout.TabCount > position)
+            {
+                _tabLayout.GetTabAt(position).Icon?
+                    .SetColorFilter(_selectedColor, PorterDuff.Mode.SrcIn);
+                _tabLayout.GetTabAt(position).Select();
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+
+        void SetupTabColors()
+        {
+            _tabLayout.SetSelectedTabIndicatorColor(_selectedColor);
+            _tabLayout.SetTabTextColors(_unselectedColor, _selectedColor);
+            for (int i = 0; i < _tabLayout.TabCount; i++)
+            {
+                var tab = _tabLayout.GetTabAt(i);
+                tab.Icon?.SetColorFilter(_unselectedColor, PorterDuff.Mode.SrcIn);
+            }
+        }
+
+        private void TabLayout_TabUnselected(object sender, TabLayout.TabUnselectedEventArgs e)
+        {
+            var tab = e.Tab;
+            tab.Icon?.SetColorFilter(_unselectedColor, PorterDuff.Mode.SrcIn);
+        }
+
+        private void TabLayout_TabSelected(object sender, TabLayout.TabSelectedEventArgs e)
+        {
+            var tab = e.Tab;
+            _viewPager.CurrentItem = tab.Position;
+            tab.Icon?.SetColorFilter(_selectedColor, PorterDuff.Mode.SrcIn);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            int lastPosition = _tabLayout.SelectedTabPosition;
+            switch (e.PropertyName)
+            {
+                case nameof(PlatformTabbedPage.BarBackgroundColor):
+                case nameof(PlatformTabbedPage.BarBackgroundApplyTo):
+                    SetBarBackgroundColor();
+                    SetupTabColors();
+                    SelectTab(lastPosition);
+                    break;
+                case nameof(PlatformTabbedPage.SelectedColor):
+                    SetSelectedColor();
+                    SetupTabColors();
+                    SelectTab(lastPosition);
+                    break;
+                default:
+                    base.OnElementPropertyChanged(sender, e);
+                    break;
+            }
+        }
+
+        private void SetSelectedColor()
+        {
+
+            if (FormsTabbedPage.SelectedColor != default(Xamarin.Forms.Color))
+                _selectedColor = FormsTabbedPage.SelectedColor.ToAndroid();
+        }
+
+        private void SetBarBackgroundColor()
+        {
+            if (FormsTabbedPage.BarBackgroundApplyTo.HasFlag(BarBackgroundApplyTo.Android))
+            {
+                _tabLayout.SetBackgroundColor(FormsTabbedPage.BarBackgroundColor.ToAndroid());
+                _unselectedColor = FormsTabbedPage.BarBackgroundColor != default(Xamarin.Forms.Color)
+                    ? FormsTabbedPage.BarBackgroundColor.Darken().ToAndroid()
+                    : DefaultUnselectedColor;
+            }
+            else
+            {
+                _tabLayout.SetBackgroundColor(_barBackgroundDefault);
+                _unselectedColor = DefaultUnselectedColor;
+            }
+        }
+    }
+}
---
+++ PlatformTabbedPage/ColorExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Messier16.Forms.Controls
+{
+    public static class ColorExtensions
+    {
+        public static Color Darken(this Color color)
+        {
+            return color.WithLuminosity(color.Luminosity * 0.7);
+        }
+    }
+}
