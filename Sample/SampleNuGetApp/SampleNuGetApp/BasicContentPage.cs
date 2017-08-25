febc302d59a2e76b4d366b8356b2f81202b59c4f
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-08-25 09:26:55 -0500

    NuGet sample app

---
+++ Sample/SampleNuGetApp/SampleNuGetApp/BasicContentPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SampleLocalApp
+{
+    public class BasicContentPage : ContentPage
+    {
+        public BasicContentPage(string title)
+        {
+            Title = title;
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label {
+                        Text = Title,
+                        FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+                    }
+                }
+            };
+        }
+    }
+}
