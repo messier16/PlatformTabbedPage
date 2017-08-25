989d8d74aa1bf3010975341f1b6eb76a043fab66
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-08-24 19:22:14 -0500

    Sample pages

---
+++ Sample/SampleLocalApp/SampleLocalApp/HomeTabbedPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Messier16.Forms.Controls;
+using Xamarin.Forms;
+
+namespace SampleLocalApp
+{
+    public class HomeTabbedPage : PlatformTabbedPage
+    {
+        public HomeTabbedPage()
+        {
+            //BarBackgroundColor = App.BarBackgroundColors[3];
+            //SelectedColor = App.SelectedColors[0];
+            //BarBackgroundApplyTo = BarBackgroundApplyTo.Android;
+
+            var page = new ConfigurationPage();// { Icon = "feed" };
+
+            Children.Add(page);
+            Children.Add(new BasicContentPage("YouTube"));// { Icon = "youtube" });
+            Children.Add(new BasicContentPage("Twitter"));// { Icon = "twitter" });
+            Children.Add(new BasicContentPage("Info"));// { Icon = "info" });
+        }
+    }
+}
